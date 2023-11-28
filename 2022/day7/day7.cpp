37bb9c6accf7dacc416320f15c15b6cd223a54b1
vliang-mbp14 <vliang621@gmail.com>
2023-11-27 23:09:50 -0800

    using currentNode ptr works

--- 2022/day7/day7.cpp
+++ 2022/day7/day7.cpp
@@ -5,6 +5,7 @@
 #include <string>
 #include <unordered_map>
 #include <unordered_set>
+#include <vector>
 using namespace std;
 
 struct Node;
@@ -63,22 +64,34 @@ void printNodes(const Node &node, int depth = 0) {
     }
 }
 
+void printDirectChilds(const Node &node) {
+
+    // Recursively print child nodes
+    for (const auto &child : node.childNodes) {
+        cout << (*child).name << endl;
+    }
+}
+
 enum class Command { CHANGE_DIRECTORY, LIST_FILES };
 
 class CommandLine {
    public:
-    unique_ptr<Node> currentNode = make_unique<Node>("/");
+    unique_ptr<Node> root = make_unique<Node>("/");
+    Node *currentNode;
+    vector<string> lines;
+    int currentLine = 1;
+    CommandLine() : currentNode(root.get()) {}
 
     unordered_map<string, Command> stringToCommand = {
         {"ls", Command::LIST_FILES},
-        {"cd ", Command::CHANGE_DIRECTORY},
+        {"cd", Command::CHANGE_DIRECTORY},
     };
 
     bool isDir(string line) { return line.find("dir") != string::npos; }
 
     string getDirectoryName(string line) {
         // 4 because dir + space is 4 characters
-        return line.substr(4, line.length() - 4);
+        return line.substr(4);
     }
 
     string getFileName(string line) {
@@ -94,28 +107,28 @@ class CommandLine {
         return std::stoi(line.substr(0, firstSpaceIndex));
     }
 
-    void buildNodes(ifstream &inputFile) {
-        std::string line;
-        while (getline(inputFile, line) && line[0] != '$') {
-            if (isDir(line)) {
+    void buildNodes() {
+        while (currentLine < lines.size() && lines[currentLine][0] != '$') {
+            if (isDir(lines[currentLine])) {
                 // If it's a directory, build a directory node
                 (*currentNode)
                     .addChild(
-                        move(make_unique<Node>(getDirectoryName(line), true)));
+                        move(make_unique<Node>(getDirectoryName(lines[currentLine]), true)));
             } else {
                 (*currentNode)
-                    .addChild(move(make_unique<Node>(getFileName(line),
-                                                     getFileSize(line))));
+                    .addChild(move(make_unique<Node>(getFileName(lines[currentLine]),
+                                                     getFileSize(lines[currentLine]))));
             }
+            currentLine++;
         }
     }
-    std::string findDirectoryToChange(std::string line) {
-        // find cd + space, which is 3 characters
-        return line.substr(3, line.length() - 3);
+    string findDirectoryToChange(std::string line) {
+        // find $ cd + space, which is 5 characters
+        string directoryToChange = line.substr(5);
+        return directoryToChange;
     }
 
-    bool isDirectoryDirectChild(std::string line) {
-        string directoryName = getDirectoryName(line);
+    bool isDirectoryDirectChild(std::string directoryName) {
         return find_if((*currentNode).childNodes.begin(),
                        (*currentNode).childNodes.end(),
                        [directoryName](const auto &nodePtr) {
@@ -124,64 +137,70 @@ class CommandLine {
                        }) != (*currentNode).childNodes.end();
     }
 
-    void processCommand(std::string command, std::ifstream &inputFile,
+    void processCommand(std::string command,
                         std::string line) {
+        cout << "processing command " << command << endl;
         auto it = stringToCommand.find(command);
         if (it != stringToCommand.end()) {
             switch (stringToCommand[command]) {
                 case Command::LIST_FILES:
                     // if it is ls, parse all the lines until the next command
-                    buildNodes(inputFile);
-                    printNodes(*currentNode);
+                    currentLine++;
+                    buildNodes();
+                    cout << "$ ls\n" << endl;
+                    printDirectChilds(*currentNode);
+                    cout << "\n" << endl;
                     break;
                 case Command::CHANGE_DIRECTORY:
                     string directory = findDirectoryToChange(line);
                     // make sure directory exists
-                    // if (isDirectoryDirectChild(directory)) {
-                    //     // change current node to what it is supposed to be
-                    //     auto foundNode =
-                    //         find_if((*currentNode).childNodes.begin(),
-                    //                 (*currentNode).childNodes.end(),
-                    //                 [directory](const auto &nodePtr) {
-                    //                     return nodePtr->name == directory &&
-                    //                            nodePtr->isDirectory;
-                    //                 });
-                    //     if (foundNode != (*currentNode).childNodes.end()) {
-                    //         currentNode.reset(
-                    //             foundNode->get());  // Assign the found node
-                    //     } else {
-                    //         std::cout << "Directory not found as a child
-                    //         node."
-                    //                   << std::endl;
-                    //     }
-                    // } else {
-                    //     std::cout << "Cannot navigate to this directory, not
-                    //     a "
-                    //                  "direct child"
-                    //               << std::endl;
-                    // }
+                    if (isDirectoryDirectChild(directory)) {
+                        // change current node to what it is supposed to be
+                        auto foundNode =
+                            find_if((*currentNode).childNodes.begin(),
+                                    (*currentNode).childNodes.end(),
+                                    [directory](const auto &nodePtr) {
+                                        return nodePtr->name == directory &&
+                                               nodePtr->isDirectory;
+                                    });
+                        if (foundNode != (*currentNode).childNodes.end()) {
+                            cout << "found node, node is " <<  (*foundNode)->name << endl;
+                            currentNode = foundNode->get();
+                        } else {
+                            cout << "Directory not found as a child node."
+                                      << endl;
+                        }
+                    } else {
+                        cout << "Cannot navigate to this directory, not a "
+                                     "direct child"
+                                  << endl;
+                    }
+                    currentLine++;
                     break;
             }
         } else {
             std::cout << "Command not found" << std::endl;
         }
     }
+
+    void process(vector<string> lines) { 
+        this->lines = lines;
+        cout << "lines[currentLine] is " << lines[currentLine] << endl;
+        while (currentLine < lines.size()) {
+            processCommand(lines[currentLine].substr(2,2), lines[currentLine]);
+        }
+    }
 };
 
 int main() {
-    std::ifstream inputFile("input.txt");
-    std::string line;
-    // skip the first line, it will always be cd /
-    std::getline(inputFile, line);
-    CommandLine cmdLine = CommandLine();
+    ifstream inputFile("input.txt");
+    string line;
+    vector<string> lines; 
     while (std::getline(inputFile, line)) {
-        // process each terminal output
-        // cd, dir, ls,
-        if (line[0] == '$') {
-            // process command if it is a command
-            cmdLine.processCommand(line.substr(2), inputFile, line);
-        }
+        lines.push_back(line);
     }
+    CommandLine cmdLine = CommandLine();
+    cmdLine.process(lines);
     // Node rootNode = Node();
     // rootNode.name = "/";
     // printNodes(*cmdLine.currentNode);
